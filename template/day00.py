de40de0292de80208bc4c6cdca7c7f966637d049
Malavika Mantripragada <malavikamantripragada@gmail.com>
2025-11-30 03:34:30 -0500

    template

---
+++ template/day00.py
@@ -0,0 +1,11 @@
+def partOne(inp):
+    return 0
+
+def partTwo(inp):
+    return 0
+
+with open('input.txt') as f:
+    file_input = f.readlines()
+
+print("Part One:", partOne(file_input))
+print("Part Two:", partTwo(file_input))
